bd94a0eb720bef6e2a14ca73ab18449a951c9dcd
Jeiman Cabarcas <jeiman@wordlist.com>
2026-05-24 08:18:54 -0500

    ssr

--- api/index.js
+++ api/index.js
@@ -1,6 +1,4 @@
-// Vercel Serverless Function entry point (CommonJS wrapper).
-// Uses dynamic import() to load the Angular SSR Express handler (.mjs ES Module).
-module.exports = async (req, res) => {
+export default async (req, res) => {
   const { reqHandler } = await import('../dist/drop-fcf/server/server.mjs');
   return reqHandler(req, res);
 };
